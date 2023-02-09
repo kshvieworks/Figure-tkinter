ba43bf492b970103d0839f57b7ebefcf78c16206
ksh <ksh@vieworks.com>
2023-02-09 11:03:38 +0900

    Version 1 (2023-02-08)

---
+++ Python/DrawingLib.py
@@ -0,0 +1,39 @@
+'''
+This File Contains the Way to Draw Figure
+Class BTNCLKEvent: Containing Interactive Functions
+Class FollowingSettings: Containing Detail Operation Functions
+Class SpecialEvent: Functions with Special Option
+'''
+
+class BTNCLKEvent:
+    def init(self):
+        asf = 1
+    def DrawCurve(self):
+        asdf = 1
+    def mkNewFig(self):
+        a =1
+
+
+class FollowingSettings:
+    def init(self):
+        asdf = 1
+    def ReadClipBoard(self):
+        asdf = 1
+    def FigureOptionSettings(self):
+        asdf = 1
+    def ForceAspect(self):
+        asdf = 1
+
+
+class SpecialEvent:
+    def init(self):
+        asdf = 1
+
+    def GaussianCurve(self):
+        asdf = 1
+
+    def DrawGaussianCurve(self):
+        asdf = 1
+
+    def GaussianFWHM(self):
+        asdf = 1
